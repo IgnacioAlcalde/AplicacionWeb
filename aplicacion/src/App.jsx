eba778a9b384b4191657126f711c8892ccbbe89d
Joel Salas <joel.salas@cloud.uautonoma.cl>
2024-10-30 21:27:00 -0300

    Creacion de cuadrilla (inicio e implementacion App)

--- aplicacion/src/App.jsx
+++ aplicacion/src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import IniciarSesion from './paginas/inicio_sesion/iniciar_sesion.jsx'
 import DirectorMunicipalidad from './paginas/inicios/director_municipalidad.jsx';
-import Inicio from "./componentes/director_municipalidad/inicio.jsx";
+import InicioMunicipalidad from "./componentes/director_municipalidad/inicio.jsx";
+import InicioCuadrilla from "./componentes/cuadrilla/inicio.jsx";
+import Cuadrilla from "./paginas/inicios/cuadrilla.jsx";
 function Aplicacion() {
     return (
       <div className='Aplicacion'>
@@ -10,8 +12,13 @@ function Aplicacion() {
           <Routes>
             <Route path="/" element={<IniciarSesion/>} />
             <Route path='director-municipalidad' element={<DirectorMunicipalidad/>}>
-              <Route path="inicio" element={<Inicio />} />
+              <Route path="inicio" element={<InicioMunicipalidad />} />
             </Route>
+
+            <Route path='cuadrilla' element={<Cuadrilla/>}>
+              <Route path="inicio" element={<InicioCuadrilla/>} />
+            </Route>
+
           </Routes>
         </Router>
       </div>
