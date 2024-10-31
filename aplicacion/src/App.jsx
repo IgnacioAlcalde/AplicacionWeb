9f5dff5f4b4b1a073d6a359fd11b633a9e083075
Francisco-Munozz <francisco.munoz13@cloud.uautonoma.cl>
2024-10-30 21:24:26 -0300

    iniciar sesion con rut agregado, se puede intercalar entre iniciar con  correo e iniciar con rut

--- aplicacion/src/App.jsx
+++ aplicacion/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import IniciarSesion from './paginas/inicio_sesion/iniciar_sesion.jsx'
+import IniciarSesionCorreo from './paginas/inicio_sesion/iniciar_sesion_correo.jsx';
+import IniciarSesionRut from './paginas/inicio_sesion/iniciar_sesion_rut.jsx';
 import DirectorMunicipalidad from './paginas/inicios/director_municipalidad.jsx';
 import Inicio from "./componentes/director_municipalidad/inicio.jsx";
 function Aplicacion() {
@@ -8,7 +9,8 @@ function Aplicacion() {
       <div className='Aplicacion'>
         <Router>
           <Routes>
-            <Route path="/" element={<IniciarSesion/>} />
+            <Route path="/" element={<IniciarSesionCorreo/>} />
+            <Route path="inicio_sesion_rut" element={<IniciarSesionRut/>} />
             <Route path='director-municipalidad' element={<DirectorMunicipalidad/>}>
               <Route path="inicio" element={<Inicio />} />
             </Route>
